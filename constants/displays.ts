0e21068cf1ff923645e7d2d033e01e399d16d3db
Barraki <barraki201@gmail.com>
2025-03-18 02:45:58 +0200

    [FEAT]: Add script for setting power mode of displays

---
+++ constants/displays.ts
@@ -0,0 +1,22 @@
+export const powerModes = [
+  {
+    name: "On",
+    value: 1,
+  },
+  {
+    name: "Standby",
+    value: 2,
+  },
+  {
+    name: "Suspend",
+    value: 3,
+  },
+  {
+    name: "Off",
+    value: 4,
+  },
+  {
+    name: "Reserved",
+    value: 5,
+  },
+]
---
+++ scripts/monitor-power.ts
@@ -0,0 +1,34 @@
+// Name: Display Power Mode
+// Description: Change the power mode of the external display
+// Author: Barraki
+
+import "@johnlindquist/kit"
+import { DisplayObject } from "../types/ddc";
+import { parseDisplayPath } from "../utils/parseDisplayPath";
+import ddcci from "@hensm/ddcci";
+import { VCPFeatureCode } from "@ddc-node/ddc-node";
+import { powerModes } from "../constants/displays";
+
+const displayObjects: DisplayObject[] = ddcci.getMonitorList().map(parseDisplayPath);
+
+const displayChoices = displayObjects.map((display, index) => ({
+  name: `${display.manufacturer}${display.model} ${index === 0 ? "(Primary)" : ""}`,
+  value: display,
+}));
+
+const selectedDisplay = await arg({
+  placeholder: "Select display to toggle power:",
+  hint: "Choose the display you want to toggle power",
+}, displayChoices);
+
+const powerMode = ddcci._getVCP(selectedDisplay.raw, VCPFeatureCode.DisplayControl.PowerMode);
+console.warn(`The current power mode: ${powerMode}`);
+
+const newPowerMode = await arg({
+  placeholder: "Enter the new power mode (1-4):",
+  hint: "Enter the new power mode (1-4)",
+}, powerModes);
+
+ddcci._setVCP(selectedDisplay.raw, VCPFeatureCode.DisplayControl.PowerMode, newPowerMode);
+
+console.log(`The new power status: ${powerModes.find((mode) => mode.value === newPowerMode)?.name}`);
